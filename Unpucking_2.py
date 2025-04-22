1f4461e5db73b3a16131c42c11b3e7455f935665
polina <polina_192837@mail.ru>
2025-04-22 21:21:43 +0300

    22. 04

--- Unpucking_2.py
+++ Unpucking_2.py
@@ -19,58 +19,74 @@
 #Реализуйте упаковку и
 #распаковку объектов класса «Самолет» с использованием
 #модуля pickle
-from pickle import dump, load
+#from pickle import dump, load
+#
+#class Airplane:
+#    def __init__(self, type, class_airplane, passancer, capacity, engine, model):
+#        self.type = type
+#        self.class_airplane = class_airplane
+#        self.passancer = passancer
+#        self.capacity = capacity
+#        self.model = model
+#        self.engine = engine
+#    def raise_chassi(self):
+#        print(f'{self.model}  lifted the chaussy')
+#
+#    def lower_class(self):
+#        print(f'{self.model} lowered their chausses')
+#
+#    def start_engine(self):
+#        print(f'{self.engine} the engines are running successfully')
+#def loader(file):
+#    try:
+#        while True:
+#            yield load(file)
+#    except:
+#        pass
+#
+#list_airplane = []
+#path = 'C:/python42/Airplane.pkl'   
+#while True:
+#    for airplane in list_airplane:
+#        print(airplane.model)
+#    choise = input('1 - add airplane\n2 - save to file\n3 - load from file')
+#    if choise == '1':
+#        type = input('input type')
+#        class_airplane =  input('input class_airplane')
+#        passancer =  input('input passancer')
+#        capacity =  input('input capacity')
+#        model =  input('input model')
+#        engine = input('input engine')
+#        airplane = Airplane(type, class_airplane, passancer, capacity, engine, model)
+#        list_airplane.append(airplane)
+#    elif choise == '2':
+#        with open(path, 'ab') as file:
+#            for airplane in list_airplane:
+#                dump(airplane, file)
+#        list_airplane.clear()
+#    elif choise == '3':
+#        with open(path, 'rb') as file:
+#            for airplane in loader(file):
+#                print(airplane.model)
+#            list_airplane.append(airplane)
 
-class Airplane:
-    def __init__(self, type, class_airplane, passancer, capacity, engine, model):
-        self.type = type
-        self.class_airplane = class_airplane
-        self.passancer = passancer
-        self.capacity = capacity
-        self.model = model
-        self.engine = engine
-    def raise_chassi(self):
-        print(f'{self.model}  lifted the chaussy')
 
-    def lower_class(self):
-        print(f'{self.model} lowered their chausses')
+# JSON
+# запись в файл dump(что выгрузить, куда выгрузить)
+# загрузка из файла load (откуда выгрузка)
+class MyClass:
+    def __init__(self, attr1, attr2, attr3):
+        self.attr1 = attr1
+        self.attr2 = attr2
+        self.attr3 = attr3
+    
+    def toJSON(self):
+        return json.damps(self, indent = 4)
 
-    def start_engine(self):
-        print(f'{self.engine} the engines are running successfully')
-def loader(file):
-    try:
-        while True:
-            yield load(file)
-    except:
-        pass
-
-list_airplane = []
-path = 'C:/python42/Airplane.pkl'   
-while True:
-    for airplane in list_airplane:
-        print(airplane.model)
-    choise = input('1 - add airplane\n2 - save to file\n3 - load from file')
-    if choise == '1':
-        type = input('input type')
-        class_airplane =  input('input class_airplane')
-        passancer =  input('input passancer')
-        capacity =  input('input capacity')
-        model =  input('input model')
-        engine = input('input engine')
-        airplane = Airplane(type, class_airplane, passancer, capacity, engine, model)
-        list_airplane.append(airplane)
-    elif choise == '2':
-        with open(path, 'ab') as file:
-            for airplane in list_airplane:
-                dump(airplane, file)
-        list_airplane.clear()
-    elif choise == '3':
-        with open(path, 'rb') as file:
-            for airplane in loader(file):
-                print(airplane.model)
-                list_airplane.append(airplane)
-
-
-
+import json
+path = 'C:/python42/My_File.pkl'
+obj1 = MyClass(23,'Python', [234, 'Hello!', True])
+with open(path,'rb') as file:
+    json.damp(obj1.toJSON(), file)
 
             
---
+++ pattern.py
@@ -0,0 +1,129 @@
+##ПАТТЕРНЫ ПРАУКТИРОВАНИЕ
+##ПОРАЖДАЮЩИЕ(Singleton, Fabric, Abstract Fabric)
+##СТРУКТУРНЫЕ(Facade)
+##ПОВЕДЕНЧИСКИЕ(Iterator)
+#
+##Создайте классическую реализацию паттерна Singleton.
+##Протестируйте работу созданного класса.
+##__init - команда определения экземпляра __new__ - команда-конструктор
+#
+#class Singleton:
+#    _instsnce = None
+#    def __new__ (cls):
+#        if cls._instsnce is None: # если экземпляра одниночки нет
+#            cls._instsnce = super(Singleton, cls).__new__(cls)  # создать экземпляр одиночку
+#        else:
+#            print('Экземпляр-одиночка уже создан')
+#        return cls._instsnce
+#    
+#class NotSingLetone:
+#    pass
+#
+#refery = Singleton()
+#print(refery)
+#refery2 = Singleton()
+#print(refery2)
+#print(refery is refery2)
+#
+#obj1 = NotSingLetone()
+#obj2 = NotSingLetone()
+#print(obj1 is obj2, obj1, obj2)
+#
+##pattern Fabric
+#
+#class Fabric:
+#    def __init__(self, name, age, profesia):
+#        self.name = name
+#        self.age = age
+#        self.profesia = profesia
+#    def get_infj(self):
+#        return f'name:{self.name}, profesia:{self.profesia}'
+#    
+#
+#class Driver(Fabric):
+#    def __init__(self, name, age, profesia):
+#        super().__init__(name, age, profesia)
+#
+#class Pilot(Fabric):
+#    def __init__(self, name, age, profesia):
+#        super().__init__(name, age, profesia)
+#
+#user1 = Driver('Dominuic Taretto', 45, 'Speedster')
+#user2 = Pilot('Alasheev', 76, 'test pilot')
+#print(user1, user2)
+#print(issubclass( Driver, Fabric), issubclass( Pilot, Fabric) )
+#
+##
+#from abc import ABC, abstractmethod
+#class AbstractFactory(ABC):
+#    @abstractmethod
+#    def create_factory(self,name):
+#        if name == 'Lada':
+#            factory = LadaFactory()
+#            return factory
+#        elif name == 'Sukhoi':
+#            factory = SukhoiFactory()
+#            return factory
+#
+#class LadaFactory(AbstractFactory):
+#    def __init__(self):
+#        self.name = 'фабрика компании лада'
+#
+#class SukhoiFactory(AbstractFactory):
+#    def __init__(self):
+#        self.name = 'фабрика компании Сухой'
+#    def create_factory(self, name):
+#        return super().create_factory(name)
+#f = SukhoiFactory()
+#factory_from_irkutsk = f.create_factory('Sukhoi')
+#
+#print(factory_from_irkutsk)
+
+#Пользователь вводит с клавиатурынабор чисел и путь
+#кфайлу для сохранения полученных данных. Необходимо:
+#■ Сохранить все полученные числа.
+#■ Найти максимум, минимум. Эти значения сохранить
+#в том же файле.
+#■ Отобразить числа.
+#■ Отобразить максимум и минимум.
+#■ Создать класс для логгирования операций. При создании объекта класса нужно уточнить куда производится
+#логгирование: экран или файл. В программе можно
+#создать только один объект класса. Все действия
+#объекта этого класса.
+
+class Logger:
+    _instance = None
+    logger_location = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls.logger_location = input('куда вести запись логирования:\n 1-в файл\n 2-в терминал')
+            cls._instance = super(Logger, cls).__new__(cls)
+        else:
+            print('лщгер уже создан')
+        return cls._instance
+    
+    def logging(self, message):
+        if self.logger_location == '2':
+            print(message)
+        else:
+            with open('Logging.txt', 'a', encoding='utf-8') as file:
+                file.write(message + '\n')
+
+    def log_input_digits(self):
+        self.logging('были добавленны числы')
+
+    def log_extrem_digits(self, maxx, minx):
+        self.logging(f'max:{maxx}, min {minx}')
+
+class Digits:
+    def __init__(self, path = 'Digits.txt', logger = None):
+        array = input('введите числа через пробел')
+        self.list_digits = list(map(int, array.split(' ')))
+        with open(path, 'a') as file:
+            for digit in self.list_digits:
+                file.write(str(digit)+'\n')
+        logger.log_input_digits()
+
+logger = Logger()
+print(logger)
+array = Digits(logger=logger)
